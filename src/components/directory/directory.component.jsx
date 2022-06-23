2112510d4cad4375fcac13c49ed3647e14b49449
parilbeladiya <103988474+parilbeladiya@users.noreply.github.com>
2022-06-23 15:09:02 +0530

    Added Routing + React Router

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -4,13 +4,13 @@ import '../directory/directory.style.scss';
 
 
 const Directory = ({categories}) => {
-    return(
-        <div className="directory-container">
-        {categories.map((category) => (
-          <CategoryItem key={category.id} category={category} />
-        ))}
-      </div>
-    );
+  return(
+    <div className="directory-container">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
 };
 
 export default Directory;
