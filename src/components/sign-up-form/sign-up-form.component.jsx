452975defdd5a44a452fc7b0183e8dfe37be72e2
parilbeladiya <103988474+parilbeladiya@users.noreply.github.com>
2022-06-27 17:17:11 +0530

    Form Input Component

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 
+import FormInput from "../form-input/form-input.component";
+
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
 
 const defaultFormFields = {
@@ -56,16 +58,17 @@ const SignUpForm = () => {
         <div>
             <h1>Sign Up with your Email and Password</h1>
             <form onSubmit={ handleSubmit }>
-                <label>Display Name</label>
-                <input
+                <FormInput
+                    label="Display Name"
                     type='text'
                     required 
                     onChange={ handleChange }
                     name='displayName'
                     value={displayName}
-                />    
-                <label>Email</label>
-                <input 
+                />   
+                 
+                <FormInput 
+                    label="Email"
                     type='email'
                     required
                     onChange={ handleChange }
@@ -73,8 +76,8 @@ const SignUpForm = () => {
                     value={email}
                 />
 
-                <label>Password</label>
-                <input 
+                <FormInput 
+                    label="Password"
                     type='password'
                     required
                     onChange={ handleChange }
@@ -82,8 +85,8 @@ const SignUpForm = () => {
                     value={password}
                 />
 
-                <label>Confirm Password</label>
-                <input 
+                <FormInput 
+                    label="Confirm Password"
                     type='password'
                     required
                     onChange={ handleChange }
