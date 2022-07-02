bdb7089fe4be6765048524fa3e0282abc96193c8
parilbeladiya <103988474+parilbeladiya@users.noreply.github.com>
2022-07-02 14:49:10 +0530

    Product card component with context

--- src/App.js
+++ src/App.js
@@ -3,13 +3,10 @@ import { Routes, Route} from 'react-router-dom';
 import Home from "./routes/home/home.component";
 import Navigation from './routes/navigation/navigation.component';
 import Authentication from './routes/authentication/authentication.component';
+import Shop from './routes/shop/shop.component';
 
 
 const App = () => {
-
-  const Shop = () => {
-    return <h1>I am the SHOP PAGE</h1>
-  }
 
   return (
     <Routes>
