14dcf1aa10e5a249b573161ace8b79f97f17a446
parilbeladiya <103988474+parilbeladiya@users.noreply.github.com>
2022-07-16 10:50:02 +0530

    Redux Persist

--- src/index.js
+++ src/index.js
@@ -4,16 +4,19 @@ import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
 import { Provider } from 'react-redux';
-import { store } from './store/store';
+import { PersistGate } from 'redux-persist/lib/integration/react';
+import { store, persistor } from './store/store';
 import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,6 @@
 import { compose, createStore, applyMiddleware, } from "redux"; 
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import { rootReducer } from "./root-reducer";
 
@@ -7,13 +9,28 @@ const loggerMiddlewars = (store) => (next) => (action) => {
         return next(action);
     }
 
+    console.log('=== Type: ', action.type);
+    console.log('=== Payload: ', action.payload);
+    console.log('=== Current State: ', store.getState());
+
     next(action);
+    console.log('=== Next State: ', store.getState());
 
 };
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    blacklist: ['user'],    
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middlewares = [ loggerMiddlewars ];
 
 const composedEnhancers = compose(applyMiddleware(...middlewares));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers ); 
+export const store = createStore(persistedReducer, undefined, composedEnhancers ); 
+
+export const persistor = persistStore(store);
 
